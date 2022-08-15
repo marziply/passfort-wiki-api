d4ad561ac1c2bbc9eff657029a2c8f335d703ca7
marziply <contact@marziply.dev>
2022-08-15 14:57:13 +0100

    Added routes mod

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-  println!("Hello, world!");
+mod routes;
+
+use rocket::{build, launch, Build, Rocket};
+use routes::{catchers, routes};
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+  build()
+    .register("/", catchers())
+    .mount("/", routes())
 }
---
+++ src/routes.rs
@@ -0,0 +1,9 @@
+use rocket::{Catcher, Route};
+
+pub(super) fn routes() -> Vec<Route> {
+  vec![]
+}
+
+pub(super) fn catchers() -> Vec<Catcher> {
+  vec![]
+}
